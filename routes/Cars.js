65be81c0a4672d651911d655ec2360f813902a2d
eduarddanaila <eduardanaila04@gmail.com>
2022-10-25 14:52:21 +0100

    hackathon day 2

--- routes/Cars.js
+++ routes/Cars.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const { carModel } = require('./db.js');
+const { carModel } = require('../db.js');
 
 router.get('/getAllCars', (req, res) => {
     carModel.find()
